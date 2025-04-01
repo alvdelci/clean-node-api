f53342da6a9f6a2af3c31a023be3c196afa920ce
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-01 11:50:18 -0300

    add jest config

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: true,
+
+  coverageDirectory: "coverage",
+
+  coverageProvider: "v8",
+};
+
+export default config;
