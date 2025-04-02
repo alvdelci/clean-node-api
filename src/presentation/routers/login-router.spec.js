6d9c3991d23adde77dc7cb938de519bb33f0d176
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-02 14:38:33 -0300

    atualizando retorno para unauthorized error

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,6 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
+const UnauthorizedError = require('../helpers/unauthorized-error')
 
 const makeSut = () => {
     class AuthUseCaseSpy { //Spy porque essa classe eh utilizada apenas para test, com mock de dados
@@ -80,5 +81,6 @@ describe('Login Router', () => {
 
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(401)
+        expect(httpResponse.body).toEqual(new UnauthorizedError())
     })
 })
