8bff09f0d1887ae06e185483e1a498eb449087b5
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-07 12:52:36 -0300

    garantindo que AuthUseCase throws se qualquer dependencia emitir erro

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -87,6 +87,16 @@ const makeUpdateAccessTokenRepository = () => {
     return new UpdateAccessTokenRepositorySpy()
 }
 
+const makeUpdateAccessTokenRepositoryWithError = () => {
+    class UpdateAccessTokenRepositorySpy {
+        async update() {
+            throw new Error()
+        }
+
+    }
+    return new UpdateAccessTokenRepositorySpy()
+}
+
 const makeSut = () => {
     const encrypterSpy = makeEncrypter()
     const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
@@ -226,6 +236,7 @@ describe('Auth UseCase', () => {
     test('should throws if any dependency throws', async () => {
         const loadUserByEmailRepository = makeLoadUserByEmailRepository()
         const encrypter = makeEncrypter()
+        const tokenGenerator = makeTokenGenerator()
 
         const suts = [].concat(
             new AuthUseCase({
@@ -239,6 +250,12 @@ describe('Auth UseCase', () => {
                 loadUserByEmailRepository,
                 encrypter,
                 tokenGenerator: makeTokenGeneratorWithError()
+            }),
+            new AuthUseCase({
+                loadUserByEmailRepository,
+                encrypter,
+                tokenGenerator,
+                updateAccessTokenRepository: makeUpdateAccessTokenRepositoryWithError()
             }),
         )
         for (const sut of suts) {
