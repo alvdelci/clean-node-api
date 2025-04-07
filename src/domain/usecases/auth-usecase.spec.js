862c2879b0a75900b5af82c7fe46d1b8ca6bebc9
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-07 11:49:36 -0300

    Modificando consturtor do AuthUseCase para receber um objeto

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -49,7 +49,11 @@ const makeSut = () => {
     const encrypterSpy = makeEncrypter()
     const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
     const tokenGeneratorSpy = makeTokenGenerator()
-    const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
+    const sut = new AuthUseCase({
+        loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+        encrypter: encrypterSpy,
+        tokenGenerator: tokenGeneratorSpy
+    })
 
     return {
         sut,
@@ -79,13 +83,13 @@ describe('Auth UseCase', () => {
     });
 
     test('should throws if no LoadUserByEmailRepository is provided', async () => {
-        const sut = new AuthUseCase()
+        const sut = new AuthUseCase({})
         const promisse = sut.auth('any_email@mail.com', 'any_password')
         expect(promisse).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
     });
 
     test('should throws if no LoadUserByEmailRepository has no load method', async () => {
-        const sut = new AuthUseCase({})
+        const sut = new AuthUseCase({ loadUserByEmailRepository: {} })
         const promisse = sut.auth('any_email@mail.com', 'any_password')
         expect(promisse).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
     });
