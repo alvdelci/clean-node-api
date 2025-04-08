d8b4343c048e9657703da834adbd141428bb748d
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-08 19:24:50 -0300

    garantindo que EmilValidator throws se um email nao for fornecido

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,6 @@
-const EmailValidator = require("./email-validator")
 const validator = require('validator')
+const EmailValidator = require("./email-validator")
+const MissingParamError = require('../errors/missing-param-error')
 //factory -> funcao que exporta um instancia de uma classe
 const makeSut = () => {
     return new EmailValidator()
@@ -23,5 +24,10 @@ describe('Email Validator', () => {
         const sut = makeSut()
         sut.isValid('any_email@mail.com')
         expect(validator.email).toBe('any_email@mail.com')
+    })
+
+    test('should throw if no email is provided', async () => {
+        const sut = makeSut()
+        expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))//Quando a funcao que vai ser testada nao e assincrona devemos passar o ponteiro dela, ja que nao retorna uma promise. Por isso a arrow fuction
     })
 })
