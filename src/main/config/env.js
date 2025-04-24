3ab969c1b818006f2790b31d3829f30df0ce01d3
alvdelci <delcimarioalvesdev@gmail.com>
2025-04-23 21:11:07 -0300

    adicionando porta padrao para a aplicacao

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
-  port: process.env.PORT
+  port: process.env.PORT || 5000
 }
